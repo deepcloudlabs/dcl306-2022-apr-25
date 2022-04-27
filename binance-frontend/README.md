b24ae42af61f13e2e536cc7cbc4b6e21a3cd7575
deepcloudlabs <deepcloudlabs@gmail.com>
2022-04-27 14:51:33 +0300

    initial commit

---
+++ binance-frontend/README.md
@@ -0,0 +1,21 @@
+# Semantic Versioning
+
+2.4.3 -> 2.4.4 -> 2.4.5
+
+2.4.1 -> 2.5.3
+
+2.4.1 -> 3.1.2
+
+2: Major Release -> Breaking Compatibility 
+
+4: Minor Release -> Backward Compatible, New Features
+
+3: Patch Release -> Backward Compatible, No new Feature
+
+~ -> Accept Patch Release
+
+^ -> Accept Minor Release
+
+~2.4.3
+
+^2.4.3 -> Accepts 2.5.1
---
+++ binance-frontend/src/App.js
@@ -0,0 +1,35 @@
+function App() {
+    return (
+        <div className="container">
+            <p></p>
+            <div className="card">
+                <div className="card-header">
+                    <h4 className="card-title">Market Console</h4>
+                </div>
+                <div className="card-body">
+                </div>
+            </div>
+            <p></p>
+            <div className="card">
+                <div className="card-header">
+                    <h4 className="card-title">Chart Data</h4>
+                </div>
+                <div className="card-body">
+
+                </div>
+            </div>
+            <p></p>
+            <div className="card">
+                <div className="card-header">
+                    <h4 className="card-title">Market Data</h4>
+                </div>
+                <div className="card-body">
+
+                </div>
+            </div>
+
+        </div>
+    );
+}
+
+export default App;
